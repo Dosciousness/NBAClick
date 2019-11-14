4ed295d29fa3cc48903077da5d4d4beff5bc51cb
Sam Boal <sboal510@gmail.com>
2019-11-14 11:06:51 -0800

    final

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>NBA Quickly Clicky Pick Me!</h1>
+      <h2>Click on an player to earn points, but don't pick the same player twice!</h2>
+    </header>
+  );
+}
+
+export default Header;
